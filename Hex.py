b13e112d6ff3450d58baf2f35e5670e775fc43d4
HEX-PLUS <91185222+SW4JON@users.noreply.github.com>
2023-09-11 19:04:48 +0600

    Create Hex.py

---
+++ Hex.py
@@ -0,0 +1,8 @@
+import os,platform
+os.system('clear')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    os.system('chmod +x Hex;./Hex')
+elif bit=='32bit':
+    os.system('chmod +x Hex32;./Hex32')
